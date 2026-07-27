15e6e387f83853495d70e0f86746c597630a98bb
merubemfigueredo-commits <me.rubemfigueredo@gmail.com>
2026-07-27 14:45:24 -0300

    Update dfc.py

--- dfc.py
+++ dfc.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 from datetime import datetime
-import openpyxl
+from openpyxl import Workbook, load_workbook
 
 st.set_page_config(page_title="Fluxo de Caixa", page_icon="💰", layout="wide")
 st.title("Demonstração do Fluxo de Caixa")
